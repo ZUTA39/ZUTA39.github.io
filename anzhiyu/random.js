2a12b18c91c08dca14778beb97bfbdb88226ca7b
ZUTA39 <1307603452@qq.com>
2024-12-20 17:23:35 +0800

    Site updated: 2024-12-20 17:23:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/25/Blog building/","2024/11/08/ComputerComposition/","2024/11/08/DataBaseStudyRecord/","2024/11/08/JavaStudyRecord/","2024/11/08/JavaTalkLater/"];function toRandomPost(){
+var posts=["2024/11/25/Blog building/","2024/11/08/ComputerComposition/","2024/11/08/JavaTalkLater/","2024/11/08/DataBaseStudyRecord/","2024/11/08/JavaStudyRecord/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
