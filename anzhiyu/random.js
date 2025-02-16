381863d8e93d736b96a3e21df09901da670957bb
ZUTA39 <1307603452@qq.com>
2025-02-16 22:33:46 +0800

    Site updated: 2025-02-16 22:33:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/08/ComputerComposition/","2024/11/08/DataBaseStudyRecord/","2024/11/08/JavaTalkLater/","2024/11/08/JavaStudyRecord/"];function toRandomPost(){
+var posts=["2024/11/08/JavaStudyRecord/","2024/11/08/ComputerComposition/","2025/01/10/ItemPet/","2024/11/08/JavaTalkLater/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
